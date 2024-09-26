3cac27620caf2c0442130918787e45a2b69cb015
Showvik Biswas <showvikbiswas@hotmail.com>
2024-09-26 16:52:43 +0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f72f0e03c20b20d93ac1.js"
+    "url": "webpack-runtime-247fa49590eaacfb3bde.js"
   },
   {
     "url": "framework-306428b3f19363da004c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f0ecaf0691a3562bb446f246b00af938"
+    "revision": "877c6e5877ab547d111730e70bbd0a56"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6f061b09cc63f0400406.js"
