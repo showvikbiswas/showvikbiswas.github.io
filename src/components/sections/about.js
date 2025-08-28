1bd68301d4d667c5036474c8eab5d0e17a5590a5
showvikbiswas@hotmail.com <showvikbiswas@hotmail.com>
2025-08-29 03:02:42 +0600

    pic update

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -198,7 +198,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/prof_pic.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
